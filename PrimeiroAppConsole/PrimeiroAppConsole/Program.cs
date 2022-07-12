c48d4ff324859faeeb4e08a7a6c666c883a0956e
Lucas Adati de Paula <80161447+adaatii@users.noreply.github.com>
2022-07-12 17:25:11 -0300

    Primeiro programa C#, aula 1 e 2.

--- PrimeiroAppConsole/PrimeiroAppConsole/Program.cs
+++ PrimeiroAppConsole/PrimeiroAppConsole/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PrimeiroAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Olá, eu sou {0} e tenho {1} anos.", nome, idade);
+        }
+
+    }
+}
